b31aabc785d4131defa70bd99649c2dd4f618c10
John Ralph Dela Rosa <you@example.com>
2025-01-22 16:12:00 +0800

    feat: create BalanceUi presentation model

---
+++ app/src/main/java/com/example/currencyexchangerapp/presentation/model/BalanceUi.kt
@@ -0,0 +1,6 @@
+package com.example.currencyexchangerapp.presentation.model
+
+/**
+ * Created by John Ralph Dela Rosa on 11/19/2025.
+ */
+data class BalanceUi(val currency: String, val amount: Double)
