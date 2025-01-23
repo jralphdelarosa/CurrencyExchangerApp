ae1bd2df4eedfa05c35a338ef4a37241408b72ac
John Ralph Dela Rosa <you@example.com>
2025-01-23 14:17:00 +0800

    feat: create Application class for Hilt initialization

---
+++ app/src/main/java/com/example/currencyexchangerapp/CurrencyExchangerApp.kt
@@ -0,0 +1,10 @@
+package com.example.currencyexchangerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by John Ralph Dela Rosa on 11/19/2025.
+ */
+@HiltAndroidApp
+class CurrencyExchangerApp: Application()
